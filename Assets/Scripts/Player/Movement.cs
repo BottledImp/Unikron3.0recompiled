db55bb97f4db8528fc57cb462b868501f7fbb05d
Imp Boy <mrlaggyboy@gmail.com>
2019-03-11 21:50:50 -0600

    AAAAAAAAAAAAAAAAAA

---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    #region Variables
+    public float speed = 3f;
+    public float jumpSpeed = 2f;
+    private Rigidbody2D rig;
+    public float inputVector;
+    public GameObject holder;
+    private bool grounded;
+    public float JumpForce = 400;
+
+    #endregion
+
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Move();
+
+
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rig.AddForce(new Vector2(rig.velocity.x, JumpForce));
+        }
+
+    }
+
+    private void Move()
+    {
+
+        inputVector = Input.GetAxis("Horizontal");
+
+        if (inputVector != 0)
+        {
+            gameObject.transform.Translate(inputVector * speed * Time.deltaTime, 0, 0);
+        }
+        if (inputVector < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (inputVector > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+
+
+   
+
+
+}
---
+++ Assets/Scripts/Player/Shoot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+    #region Variables
+    public GameObject projectile;
+    private float time;
+    public GameObject pivot;
+    public float startTime;
+    #endregion
+
+    private void Update() {
+        ShootProjectile();
+    }
+
+    private void ShootProjectile () {
+        Vector3 dir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rot = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot - 90); //Add offset
+        if (time <= 0) {
+            if (Input.GetMouseButtonUp(0)) {
+                Instantiate(projectile, pivot.transform.position, pivot.transform.rotation);
+                time = startTime;
+            }
+        }
+        else {
+            time -= Time.deltaTime;
+        }
+    }
+
+}
