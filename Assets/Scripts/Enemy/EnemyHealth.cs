391aae46032219953178e527794d737c76229efb
Imp Boy <mrlaggyboy@gmail.com>
2019-03-11 22:17:16 -0600

    PLEASE DO NOT EXPLODE

---
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour {
+
+    public float health = 100f;
+
+    private void Update() {
+        Die();
+    }
+
+    private void Die () {
+        if (health <= 0) {
+            //Animación
+            Destroy(gameObject, 0.5f); //Destruye el objeto, con un tiempo de delay (Objeto, delay)
+        }
+    }
+
+}
---
+++ Assets/Scripts/Player/Projectile/ProjectileManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileManager : MonoBehaviour {
+
+    #region Variables
+    public float speed = 5f;
+    public float fadeTime = 5f;
+    private float cntr = 0;
+    public float dmg = 10f;
+    public GameObject Effect;
+    #endregion
+
+    private void Start() {
+        
+    }
+
+    private void Update() {
+        Fade();
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
+        Damage();
+    }
+
+    private void Fade () {
+        cntr += Time.deltaTime;
+        if (cntr > fadeTime) {
+            Destroy(gameObject);
+        }
+    }
+    //Mask for environment and enemy
+    public LayerMask mask;
+    private void Damage () {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up, 0.5f, mask);
+        if (hit.collider.CompareTag("Enemy")) {
+            hit.collider.GetComponent<EnemyHealth>().health -= 10;
+            //Instantiate(Effect, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+        else {
+            Destroy(gameObject);
+        }
+    }
+
+}
