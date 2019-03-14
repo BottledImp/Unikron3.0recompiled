df01e10cf5162016f1f73a6c5a77fa81b01d4f69
Imp Boy <mrlaggyboy@gmail.com>
2019-03-14 13:00:45 -0600

    Added player health and conditionals for animations

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -4,30 +4,44 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    #region Variables
+    #region Floats
     public float speed = 3f;
+    float movespeed = 0f;
     public float jumpSpeed = 2f;
     public float crouchspeed = 1f;
-    private Rigidbody2D rig;
     public float inputVector;
-    public GameObject holder;
+    public float Playerhealth = 100f
+    
+
+    
+    public bool isCrouching;
+    public bool isSliding;
     private bool grounded;
+      
+
+
+    private Rigidbody2D rig;
+    public GameObject holder;
     public BoxCollider2D Boxcollider;
     #endregion
+
+
+
+
 
     private void Start()
     {
         rig = GetComponent<Rigidbody2D>();
     }
 
+
     private void Update()
     {
-        print(inputVector);
         Move();
         Crouch();
-        print(inputVector);
         Run();
         Jump();
+
         
     }
 
@@ -35,18 +49,33 @@ public class Movement : MonoBehaviour
 
     private void Move()
     {
-        
-        inputVector = Input.GetAxis("Horizontal");
 
-        if (inputVector != 0)
+        if (Input.GetKeyDown(KeyCode.A))
         {
-            gameObject.transform.Translate(inputVector * speed * Time.deltaTime, 0, 0);
+            movespeed = -speed;
         }
-        if (inputVector < 0)
+        else if (Input.GetKeyUp(KeyCode.A))
+        {
+            movespeed = 0;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            movespeed = speed;
+        }
+        else if (Input.GetKeyUp(KeyCode.D))
+        {
+            movespeed = 0;
+        }
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(movespeed * crouchspeed, GetComponent<Rigidbody2D>().velocity.y);
+
+        if (movespeed < 0)
         {
             transform.localScale = new Vector3(-1, 1, 1);
         }
-        else if (inputVector > 0)
+        else if (movespeed > 0)
         {
             transform.localScale = new Vector3(1, 1, 1);
         }
@@ -71,17 +100,27 @@ public class Movement : MonoBehaviour
     private void Crouch()
     {
         
-        if (Input.GetKeyDown(KeyCode.LeftControl))
+        if (Input.GetKeyDown(KeyCode.S))
         {
-
             if (grounded)
             {
                 Boxcollider.enabled = false;
+                isCrouching = true;
             }
         }
-        if (Input.GetKeyUp(KeyCode.LeftControl))
+        if (Input.GetKeyUp(KeyCode.S))
         {
             Boxcollider.enabled = true;
+            isCrouching = false;
+        }
+
+        if (isCrouching && !isSliding)
+        {
+            crouchspeed = 0.5f;
+        }
+        else if (!isCrouching && !isSliding)
+        {
+            crouchspeed = 1f;
         }
     }
 
@@ -98,13 +137,26 @@ public class Movement : MonoBehaviour
         {
             speed = speed / 2;
         }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.S))
+        {
+            isSliding = true;
+        }
+        else
+        {
+            isSliding = false;
+        }
     }
+
+
 
     private void Jump() {
         
         if (Input.GetKeyDown(KeyCode.Space) && grounded) {
             rig.AddForce(new Vector2(0, jumpSpeed), ForceMode2D.Impulse);
+
         }
+         
 
     }
 
